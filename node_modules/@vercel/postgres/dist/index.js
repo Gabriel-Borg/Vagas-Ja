5645705fd882829dec5b68bbcf8693846ec22d9e
Gabriel-Borg <boydamooca16@gmail.com>
2024-10-27 22:45:45 -0300

    db vercel

---
+++ node_modules/@vercel/postgres/dist/index.js
@@ -0,0 +1,21 @@
+import {
+  VercelClient,
+  VercelPool,
+  createClient,
+  createPool,
+  db,
+  postgresConnectionString,
+  sql,
+  types
+} from "./chunk-7IR77QAQ.js";
+export {
+  VercelClient,
+  VercelPool,
+  createClient,
+  createPool,
+  db,
+  postgresConnectionString,
+  sql,
+  types
+};
+//# sourceMappingURL=index.js.map
